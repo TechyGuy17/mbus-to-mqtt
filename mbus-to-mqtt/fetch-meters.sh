45cf701d460ab59949e8b30b6445b9c332f72e40
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2024-11-27 13:26:52 +0100

    Update fetch-meters.sh
    Adding support for multiple devices

--- mbus-to-mqtt/fetch-meters.sh
+++ mbus-to-mqtt/fetch-meters.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 CONFIG_PATH=/data/options.json
 
-METER=$(jq --raw-output '.mbus_address // empty' $CONFIG_PATH)
+METERS=$(jq --raw-output '.mbus_address // empty' $CONFIG_PATH)
 PORT=$(jq --raw-output '.mbus_gateway_port // empty' $CONFIG_PATH)
 DEVICE=$(jq --raw-output '.mbus_gateway // empty' $CONFIG_PATH)
 MQTT_HOST=$(jq --raw-output '.mqtt_host // empty' $CONFIG_PATH)
@@ -9,10 +9,13 @@ MQTT_USER=$(jq --raw-output '.mqtt_user // empty' $CONFIG_PATH)
 MQTT_PASS=$(jq --raw-output '.mqtt_pass // empty' $CONFIG_PATH)
 MQTT_TOPIC=$(jq --raw-output '.mqtt_topic // empty' $CONFIG_PATH)
 
+METERARRAY=($(echo $METERS | tr ";" "\n"))
 
 echo -e "\n $(date)"
 echo "Sending data to host $MQTT_HOST as user '$MQTT_USER' using topic '$MQTT_TOPIC/'."
 
+for METER in "${METERARRAY[@]}"
+do
     echo -n "Getting data from $METER..."
     # The sed is for replacing the @ with _ to be able to match on it in HASS templates
     METER_DATA=$(mbus-tcp-request-data-multi-reply $DEVICE $PORT $METER | xq . | sed -e "s/@/_/")
@@ -20,4 +23,6 @@ echo "Sending data to host $MQTT_HOST as user '$MQTT_USER' using topic '$MQTT_TO
         -t $MQTT_TOPIC/$METER -m "${METER_DATA}"
     BYTCNT=$(echo "$METER_DATA" | wc -c)
     echo "  $BYTCNT bytes sent"
+done
+
     
